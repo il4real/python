e701ed0ba3e6522e012cc3a17fc655c4f23e16f4
il4real <iljajantikov@inbox.lv>
2022-05-02 11:59:01 +0300

    is_valid added

--- ugadajka.py
+++ ugadajka.py
@@ -1,11 +1,22 @@
 from random import randint
 
-print("Игра - 'Угадайка число!'")
-name = input('Введите Ваше имя: ')
 a = randint(1, 100)
+print("'Добро пожаловать в числовую угадайку'")
+name = input('Введите Ваше имя: ')
+
+
+def is_valid(num):
+    if int(num) in range(1, 101):
+        return int(num)
+    else:
+        return False
+
 
 while True:
     guess = int(input(f'Уважаемый(ая) {name}, Введите число от 1 до 100: '))
+    if is_valid(guess) is False:
+        print('not in range, once more')
+        continue
     if guess > a:
         print('Слишком МНОГО, попробуйте еще раз!')
     elif guess < a:
