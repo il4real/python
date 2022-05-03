d26384f8d97dec44699be68664604b50d2a7fb9f
il4real <iljajantikov@inbox.lv>
2022-05-03 09:24:21 +0300

    pass_generator.py added

---
+++ pass_generator.py
@@ -0,0 +1,36 @@
+import random
+
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+chars = ''
+ambig = 'il1Lo0O'
+
+pass_q = input('How many passwords do you need? Please enter number: ')
+passlen = input('Enter password length in numbers. (For example: 5): ')
+pass_dig = input('Do you want to add digits to your password? Type y(yes) or n(no): ')
+pass_up = input('Do you want to add upperscase letters to your password? Type y(yes) or n(no): ')
+pass_low = input('Do you want to add lowercase letters to your password? Type y(yes) or n(no): ')
+pass_ambig = input('Do you want to remove ambiguity symbols your password? Type y(yes) or n(no): ')
+
+if pass_dig == 'y':
+    chars += digits
+if pass_up == 'y':
+    chars += uppercase_letters
+if pass_low == 'y':
+    chars += lowercase_letters
+if pass_ambig == 'y':
+    for c in 'il1Lo0O':
+        chars.replace(c, '')
+
+
+def generate_password(length, char):
+    new_pass = ''
+    for i in range(int(passlen)):
+        new_pass += random.choice(chars)
+    return new_pass
+
+
+for i in range(int(pass_q)):
+    print(generate_password(passlen, chars))
